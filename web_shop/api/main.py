9f96d35484c9e051927655d578e8221ac89264d5
arusalovskiy <alexandr.rusalovskyy@gmail.com>
2020-05-08 14:41:56 +0300

    checking gunicorn api

--- web_shop/api/main.py
+++ web_shop/api/main.py
@@ -5,7 +5,7 @@ from .resources import *
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(CategoryResource, 'category', '/category/<slug>')
-api.add_resource(ProductResource, 'product', '/product/<slug>')
-api.add_resource(CustomerResource, 'customer', '/customer/<id>')
+api.add_resource(CategoryResource, '/category', '/category/<slug>')
+api.add_resource(ProductResource, '/product', '/product/<slug>')
+api.add_resource(CustomerResource, '/customer', '/customer/<id>')
 api.add_resource(OrderResource, '/order', '/order/<id>')
